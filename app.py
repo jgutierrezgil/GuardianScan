e632b7ffccc64aca710cb1df7413a5292545fead
jgutierrezgil <joaquingutierrezgil@gmail.com>
2025-02-05 20:23:09 +0100

    Enhance XSS and SQL injection checks to return successful payloads and update HTML to display results

--- app.py
+++ app.py
@@ -49,32 +49,42 @@ def leer_archivo_a_lista(ruta_archivo):
 def check_xss(url):
     """
     Por cada payload en la lista de payloads de XSS, realiza una petición GET a la URL
+    y acumula los payloads exitosos en una lista.
     """
     XSS_PAYLOADS = leer_archivo_a_lista('payloads/xss_payloads.txt')
+    xss_payload_success = []
     for payload in XSS_PAYLOADS:
         try:
-            response = requests.get(url + payload)
+            full_url = url + payload
+            response = requests.get(full_url)
             if payload in response.text:
-                
-                return True
-        except:
-            pass
+                xss_payload_success.append(payload)
+                print(f"PAYLOADS XSS EXITOSOS: {xss_payload_success}")
+        except Exception as e:
+            print(f"Error al probar el payload '{payload}': {e}")
+    if xss_payload_success:
+        return xss_payload_success
     return False
+
 
 def check_sqli(url):
     """
     Por cada payload en la lista de payloads de SQL Injection, realiza una petición GET a la URL
     """
     SQLI_PAYLOADS = leer_archivo_a_lista('payloads/sqli_payloads.txt')
+    sqli_payload_success = []
     error_keywords = ["error", "syntax", "sql"]
     for payload in SQLI_PAYLOADS:
         try:
             response = requests.get(url + payload)
             content = response.text.lower()
             if any(keyword in content for keyword in error_keywords):
-                return True
-        except:
-            pass
+                sqli_payload_success.append(payload)
+                print(f"PAYLOADS SQLI EXITOSOS: {sqli_payload_success}")
+        except Exception as e:
+            print(f"Error al probar el payload '{payload}': {e}")
+    if sqli_payload_success:
+        return sqli_payload_success
     return False
 
 if __name__ == '__main__':
